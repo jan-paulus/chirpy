30d9912b064453051df1cf96f8895d2805e4ba3c
Jan Paulus <mail@janpaulus.de>
2024-04-07 10:51:04 +0200

    add support for author_id query parameter in get chirps endpoint

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -4,17 +4,39 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/jan-paulus/chirpy/internal/database"
 )
 
-func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, _ *http.Request) {
+func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	s := r.URL.Query().Get("author_id")
+
 	chirps, err := cfg.DB.GetChirps()
 	if err != nil {
 		log.Printf("Error fetching chirps from db: %s", err)
-		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, chirps)
+	if s == "" {
+		respondWithJSON(w, http.StatusOK, chirps)
+	}
+
+	authorId, err := strconv.Atoi(s)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	filteredChirps := []database.Chirp{}
+
+	for _, chrip := range chirps {
+		if chrip.AuthorId == authorId {
+			filteredChirps = append(filteredChirps, chrip)
+		}
+	}
+
+	respondWithJSON(w, http.StatusOK, filteredChirps)
 }
 
 func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
